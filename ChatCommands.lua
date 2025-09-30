51b6e78ead70602af1a3f49b597a8884f8479aa2
avraampetrov11-creator <avraam.petrov.11@inbox.ru>
2025-09-30 19:40:51 +0500

    Update ChatCommands.lua

--- ChatCommands.lua
+++ ChatCommands.lua
@@ -1,48 +1,31 @@
--- LocalScript inside StarterPlayerScripts
+-- I WARNED, YOU ALL. YOU'VE IGNORED NOW YOUL LIFES WILL FALL. AS FOR. MY NAME, REPUTATIONS ARLEADY BEEN STAINED. AW SHUCKS!
+-- Plasmablake
+
 local Players = game:GetService("Players")
 local TextChatService = game:GetService("TextChatService")
-
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Core_Replication = ReplicatedStorage.Events.Core_Replication
 local LocalPlayer = Players.LocalPlayer
 
--- Create a custom command (/speed)
-local speedCommand = Instance.new("TextChatCommand")
-speedCommand.Name = "SpeedCommand"
-speedCommand.PrimaryAlias = "/speed"
-speedCommand.SecondaryAlias = "/walkspeed"
-speedCommand.Parent = TextChatService
+local CreateNewMeshCMD = Instance.new("TextChatCommand")
+CreateNewMeshCMD.Name = "SpeedCommand"
+CreateNewMeshCMD.PrimaryAlias = "!CreateNewMesh"
+CreateNewMeshCMD.Parent = TextChatService
 
--- Handle when command is triggered
-speedCommand.Triggered:Connect(function(textSource, message)
-    -- Only run for the local player
+CreateNewMeshCMD.Triggered:Connect(function(textSource, message)
     if textSource.UserId == LocalPlayer.UserId then
         local args = string.split(message, " ")
-        table.remove(args, 1) -- remove the alias (/speed)
+        table.remove(args, 1))
 
-        local amount = tonumber(args[1]) or 16
-        local char = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-        local humanoid = char:FindFirstChildOfClass("Humanoid")
-        if humanoid then
-            humanoid.WalkSpeed = amount
+        local PlayerName = args[1]
+        local AffectedPlayer = game.Players:FindFirstChild(PlayerName)
+
+        if AffectedPlayer then
+            print("Found player: " .. affectedPlayer.Name)
+        else
+            warn("Player not found: " .. tostring(playerName))
         end
     end
 end)
 
--- Another example: /jump
-local jumpCommand = Instance.new("TextChatCommand")
-jumpCommand.Name = "JumpCommand"
-jumpCommand.PrimaryAlias = "/jump"
-jumpCommand.Parent = TextChatService
 
-jumpCommand.Triggered:Connect(function(textSource, message)
-    if textSource.UserId == LocalPlayer.UserId then
-        local args = string.split(message, " ")
-        table.remove(args, 1)
-
-        local amount = tonumber(args[1]) or 50
-        local char = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-        local humanoid = char:FindFirstChildOfClass("Humanoid")
-        if humanoid then
-            humanoid.JumpPower = amount
-        end
-    end
-end)
